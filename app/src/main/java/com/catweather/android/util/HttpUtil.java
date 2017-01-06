dae38f1edfaf1c4d9de1d45d8ecb5b40e0f7f8f4
pengpeng <1125281536@qq.com>
2017-01-06 17:55:22 +0800

    加入创建处理省，市，县处理数据的工具类

---
+++ app/src/main/java/com/catweather/android/util/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.catweather.android.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * 创建时间: 2017/1/6 11:21
+ * 编写人:彭鹏
+ * 功能描述:
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request);
+    }
+}
